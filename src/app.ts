f6fcd3f8fcaf3e48a2456cdc83129e427bcae706
Lowdata <ayush.pahuja090@gmail.com>
2024-07-28 23:36:51 +0530

    add auth services

--- src/app.ts
+++ src/app.ts
@@ -4,4 +4,8 @@ import express from 'express';
 const app = express();
 app.use(express.json());
 
+app.get('/', (req,res)=>{
+    res.send("Hello")
+})
+
 export default app;
---
+++ src/services/authServices.ts
@@ -0,0 +1,33 @@
+import bcrypt from 'bcryptjs';
+import jwt from 'jsonwebtoken';
+import User from '../model/User';
+import { tokenToString } from 'typescript';
+
+const users: User[] = [];
+
+export const register = async(
+    email:string, 
+    password:string, 
+    name:string):Promise<User>=>{
+
+        const hashedPassword = await bcrypt.hash(password,10);
+        const newUser = {email, password:hashedPassword,name};
+        users.push(newUser);
+        return newUser;
+};
+
+
+export const login = async (email: string, password: string): Promise<string> => {
+    const user = users.find(user => user.email === email);
+    if (!user) throw new Error('User not found');
+
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) throw new Error('Invalid credentials');
+
+    const token = jwt.sign(
+        { email: user.email },
+        process.env.JWT_SECRET!,
+        { expiresIn: '1h' }
+    );
+    return token;
+};
