e7a6e12de56753e3e98367831d4ed5e3b066cdc7
Lowdata <ayush.pahuja090@gmail.com>
2024-07-29 10:31:37 +0530

    add route

--- src/routes/authRoutes.ts
+++ src/routes/authRoutes.ts
@@ -1,13 +1,11 @@
 import { Router } from 'express';
-import { registerUser, loginUser } from '../controller/authController';
+import { registerUser, loginUser, getUser } from '../controller/authController';
 import { authMiddleware } from '../middleware/authMiddleware';
 
 const router = Router();
 
 router.post('/register',registerUser);
 router.post('/login', loginUser);
-router.get('/protected', authMiddleware, (req, res) => {
-    res.status(200).json({ message: "This is a protected route." });
-});
+router.get('/user', authMiddleware, getUser); 
 
 export default router;
